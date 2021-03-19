2ab38340418a0bcb2b58dd274cc7a0d9857f35d6
Rodrigo Eustáquio Duque <32581219+rodrigoeduque@users.noreply.github.com>
2021-03-18 21:33:13 -0300

    Iniciando controle de fluxo e o paradigma procedural

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Bem Vindo ao ByteBank")
+    val titular: String = "Rodrigo Eustáquio Duque"
+    val numeroConta: Int = 100048
+    var saldo: Double = 0.0
+    saldo = 100.0 + 2
+    saldo += 200
+
+    println("Titular: $titular")
+    println("Número da Conta: $numeroConta")
+    println("Saldo da Conta: $saldo")
+}
